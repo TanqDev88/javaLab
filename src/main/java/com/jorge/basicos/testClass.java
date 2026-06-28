8aebf957b6b4cab410dcbea3bef34f070eef31ac
TanqDev88 <oliverprim783@gmail.com>
2026-06-27 23:56:21 -0300

    feat:test class added

--- src/main/java/com/jorge/basicos/testClass.java
+++ src/main/java/com/jorge/basicos/testClass.java
@@ -1,4 +1,13 @@
 package com.jorge.basicos;
 
 public class testClass {
+
+    public static String evenOrOdd(int number) {
+
+        if (number % 2 == 0) {
+            return "Even";
+        }
+
+        return "Odd";
+    }
 }
