dd7799bf8ad2807e49860e022e622453b3fb864a
TanqDev88 <oliverprim783@gmail.com>
2026-06-28 23:58:44 -0300

    build: initialize Maven project structure

--- src/main/java/com/jorge/basicos/Exercise01EvenOrOdd.java
+++ src/main/java/com/jorge/basicos/Exercise01EvenOrOdd.java
@@ -1,8 +1,8 @@
 package com.jorge.basicos;
 
-public class testClass {
+public class Exercise01EvenOrOdd{
 
-    public static String evenOrOdd(int number) {
+    public static String Exercise01EvenOrOdd(int number) {
 
         if (number % 2 == 0) {
             return "Even";
